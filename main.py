f69475f75102498cb13731c1624c0ad32a65dee3
Eric Switzer <eric.switzer@gmail.com>
2016-12-02 14:42:25 -0500

    more cleanup

--- main.py
+++ main.py
@@ -1,66 +1,75 @@
-import ConfigParser, os
-import pysm_synchrotron,pysm_thermaldust,pysm_cmb,pysm_spinningdust, pysm_noise, pysm_freefree
+"""Commandline utility for python sky model
+"""
+import os
+import argparse
+import ConfigParser
+import pysm_synchrotron, pysm_thermaldust, pysm_cmb, pysm_spinningdust, pysm_noise, pysm_freefree
 from pysm import output, config2list, file_path, write_output_single
 import healpy as hp
 import numpy as np
-import argparse
 
-if __name__ == '__main__':
 
+def main():
     parser = argparse.ArgumentParser(description='Code to simulate galactic foregrounds.')
     parser.add_argument('config_file', help='Main configuration file.')
 
-##Get the output directory and save the configuration file.
+    # Get the output directory and save the configuration file.
     Config = ConfigParser.ConfigParser()
     Config.read(parser.parse_args().config_file)
     out = output(Config._sections['GlobalParameters'])
 
-    if not os.path.exists(out.output_dir): os.makedirs(out.output_dir)
-    with open(out.output_dir+out.output_prefix+'main_config.ini','w') as configfile: Config.write(configfile)
+    if not os.path.exists(out.output_dir):
+        os.makedirs(out.output_dir)
 
-    if out.debug == True:
+    cfg_fname = out.output_dir + out.output_prefix + 'main_config.ini'
+    with open(cfg_fname, 'w') as configfile:
+        Config.write(configfile)
 
-##Print information about the run:
+    if out.debug is True:
         print '----------------------------------------------------- \n'
-        print ''.join("%s: %s \n" % item   for item in vars(out).items())
+        print ''.join("%s: %s \n" % item for item in vars(out).items())
         print '-----------------------------------------------------'
-        
-    sky = np.zeros([3,len(out.output_frequency),hp.nside2npix(out.nside)])
+
+    model = np.zeros([3, len(out.output_frequency), hp.nside2npix(out.nside)])
     print '----------------------------------------------------- \n'
-#Create synchrotron, dust, AME,  and cmb maps at output frequencies then add noise.
     if 'synchrotron' in out.components:
-        sky = sky + pysm_synchrotron.main(parser.parse_args().config_file)
+        model += pysm_synchrotron.main(parser.parse_args().config_file)
 
     if 'thermaldust' in out.components:
-        sky = sky + pysm_thermaldust.main(parser.parse_args().config_file)
+        model += pysm_thermaldust.main(parser.parse_args().config_file)
 
     if 'spinningdust' in out.components:
-        sky = sky + pysm_spinningdust.main(parser.parse_args().config_file)
+        model += pysm_spinningdust.main(parser.parse_args().config_file)
 
     if 'freefree' in out.components:
-        sky = sky + pysm_freefree.main(parser.parse_args().config_file)
+        model += pysm_freefree.main(parser.parse_args().config_file)
 
     if 'cmb' in out.components:
-        sky = sky + pysm_cmb.main(parser.parse_args().config_file)
+        model += pysm_cmb.main(parser.parse_args().config_file)
 
     if out.smoothing:
         print 'Smoothing output maps.'
         print '----------------------------------------------------- \n'
-        for i in xrange(len(out.output_frequency)): 
-            sky[:,i,:]=hp.smoothing(sky[:,i,:],fwhm=(np.pi/180./60.)*out.fwhm[i],verbose=False)
+        for i in xrange(len(out.output_frequency)):
+            fwhm = (np.pi / 180. / 60.) * out.fwhm[i]
 
-    if out.instrument_noise == True:
-        sky = sky + pysm_noise.instrument_noise(parser.parse_args().config_file)
+            model[:, i, :] = hp.smoothing(model[:, i, :],
+                                          fwhm=fwhm, verbose=False)
 
-    comps =str()
+    if out.instrument_noise is True:
+        model += pysm_noise.instrument_noise(parser.parse_args().config_file)
 
-    if out.instrument_noise: 
+    if out.instrument_noise:
         out.components.append('noise')
 
-    sky = np.swapaxes(sky,0,1)
-    for i in xrange(len(out.output_frequency)): write_output_single(sky[i,...],out,Config,i)
-    
+    model = np.swapaxes(model, 0, 1)
+    for i in xrange(len(out.output_frequency)):
+        write_output_single(model[i, ...], out, Config, i)
+
     print '-----------------------------------------------------\n'
-    print 'PySM completed successfully. \n' 
+    print 'PySM completed successfully. \n'
     print '-----------------------------------------------------'
 
+
+if __name__ == '__main__':
+    main()
